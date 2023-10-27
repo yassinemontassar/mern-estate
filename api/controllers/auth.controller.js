6ce3c5c17a34e273d33b4dac8e27e1a02e52bd01
yassinemontassar <sawthegamer70@gmail.com>
2023-10-27 13:29:41 +0200

    updated cookie duration

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,10 @@ export const google = async (req, res, next) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = user._doc;
         res
-          .cookie('access_token', token, { httpOnly: true })
+          .cookie('access_token', token, { 
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+           })
           .status(200)
           .json(rest);
       } else {
